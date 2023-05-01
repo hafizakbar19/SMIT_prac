96010fc827e8a0b98d0f56873eea73ba9d29a279
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-01 17:51:06 +0500

    ch9-11

--- ch9-11/app.js
+++ ch9-11/app.js
@@ -128,7 +128,7 @@ let check4 = () => {
     let subj1 = prompt("Enter subject 1 Name:");
     let subj2 = prompt("Enter subject 2 Name:");
     let subj3 = prompt("Enter subject 3 Name:");
-    let totalMarks = 100;
+    let totalMarks = 100*3;
     let obtMarkSubj1 = +prompt("Obtained marks in " + subj1);
     let obtMarkSubj2 = +prompt("Obtained marks in " + subj2);
     let obtMarkSubj3 = +prompt("Obtained marks in " + subj3);
@@ -140,20 +140,20 @@ let check4 = () => {
     let totalObtMarks = obtMarkSubj1 + obtMarkSubj2 + obtMarkSubj3;
     let totalPercentage = (totalObtMarks / 300) * 100;
   
-    let grade1, remarks1;
+    let grade, remarks;
   
-    if (result1 >= 80) {
-      grade1 = "A-one";
-      remarks1 = "Excellent";
-    } else if (result1 >= 70) {
-      grade1 = "A";
-      remarks1 = "Good";
-    } else if (result1 >= 60) {
-      grade1 = "B";
-      remarks1 = "You need to improve";
+    if (totalPercentage >= 80) {
+      grade = "A-one";
+      remarks = "Excellent";
+    } else if (totalPercentage >= 70) {
+      grade = "A";
+      remarks = "Good";
+    } else if (totalPercentage >= 60) {
+      grade = "B";
+      remarks = "You need to improve";
     } else {
-      grade1 = "Fail";
-      remarks1 = "Sorry";
+      grade = "Fail";
+      remarks = "Sorry";
     }
   
     document.getElementById("task6").innerHTML =
@@ -165,13 +165,88 @@ let check4 = () => {
       totalObtMarks +
       "<br>" +
       "Grade: " +
-      grade1 +
+      grade +
       "<br>" +
       "Percentage: " +
       totalPercentage +
       "%<br>" +
       "Remarks: " +
-      remarks1 +
+      remarks +
       "<br><br><br>";
+  };
+  
+  //task 7
+
+  let guess = () => {
+    let secretNum = Math.round(Math.random() * 10);
+    let guessNum = +prompt("Guess the secret number:");
+    if (guessNum === secretNum) {
+      document.getElementById("task7").innerHTML = "Bingo! Correct answer";
+    } else if (guessNum === secretNum + 1) {
+      document.getElementById("task7").innerHTML =
+        "Close enough to the correct answer";
+    } else {
+      document.getElementById("task7").innerHTML = "Wrong answer";
+    }
+  };
+
+  //task 8
+
+  let check5 = () => {
+    let num = +prompt("Enter number:");
+    if (num % 3 === 0) {
+      document.getElementById("task8").innerHTML = "Number is divisible by 3";
+    } else {
+      document.getElementById("task8").innerHTML = "Number is not divisible by 3";
+    }
+  };
+
+  //task 9
+
+  let check6 = () => {
+    let num = +prompt("Enter number:");
+    if (num % 2 === 0) {
+      document.getElementById("task9").innerHTML = "Number is even";
+    } else {
+      document.getElementById("task9").innerHTML = "Number is odd";
+    }
+  };
+
+  //task 10
+
+  let check7 = () => {
+    let temp = +prompt("Enter temperature:");
+    if (temp > 40) {
+      document.getElementById("task10").innerHTML = "It is too hot outside.";
+    } else if (temp > 30) {
+      document.getElementById("task10").innerHTML = "The Weather today is Normal.";
+    } else if (temp > 20) {
+      document.getElementById("task10").innerHTML = "Today’s Weather is cool.";
+    } else {
+      document.getElementById("task10").innerHTML = "OMG! Today’s weather is so Cool.";
+    }
+  };
+
+  //task 11
+
+  let calc = () => {
+    let num1 = +prompt("Enter first number:");
+    let num2 = +prompt("Enter second number:");
+    let operator = prompt("Enter operator:");
+    let result;
+    if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else if (operator === "%") {
+      result = num1 % num2;
+    } else {
+      result = "Invalid operator";
+    }
+    document.getElementById("task11").innerHTML ="The Answer is = " + result;
   };
   
